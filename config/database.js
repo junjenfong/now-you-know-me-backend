199b4678c09a675b1634565e83bde576dbb8d9ea
Jun Jen <jj.fong@zchwantech.com>
2025-08-20 01:12:18 +0800

    remove unnecesary code

--- config/database.js
+++ config/database.js
@@ -5,11 +5,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGODB_URI || "mongodb://localhost/now-you-know-me",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      process.env.MONGODB_URI || "mongodb://localhost/now-you-know-me"
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
